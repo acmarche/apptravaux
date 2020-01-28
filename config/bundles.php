2b971441b7c7676102ab49863a9938426ecf0f6d
jfsenechal <jfsenechal@gmail.com>
2020-01-28 11:34:17 +0100

    migrate to sf 5

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Knp\DoctrineBehaviors\Bundle\DoctrineBehaviorsBundle::class => ['all' => true],
 ];
--- src/Migrations/update.sql
+++ src/Migrations/update.sql
@@ -1,11 +1,11 @@
-ALTER TABLE `intervention` CHANGE `created` `createdAt` DATETIME NOT NULL;
+ALTER TABLE `intervention` CHANGE `created` `created_at` DATETIME NOT NULL;
 
-ALTER TABLE `intervention` CHANGE `updated` `updatedAt` DATETIME NOT NULL;
+ALTER TABLE `intervention` CHANGE `updated` `updated_at` DATETIME NOT NULL;
 
-ALTER TABLE `suivis` CHANGE `created` `createdAt` DATETIME NOT NULL;
+ALTER TABLE `suivis` CHANGE `created` `created_at` DATETIME NOT NULL;
 
-ALTER TABLE `suivis` CHANGE `updated` `updatedAt` DATETIME NOT NULL;
+ALTER TABLE `suivis` CHANGE `updated` `updated_at` DATETIME NOT NULL;
 
-ALTER TABLE `avaloir` CHANGE `created` `createdAt` DATETIME NOT NULL;
+ALTER TABLE `avaloir` CHANGE `created` `created_at` DATETIME NOT NULL;
 
-ALTER TABLE `avaloir` CHANGE `updated` `updatedAt` DATETIME NOT NULL;
+ALTER TABLE `avaloir` CHANGE `updated` `updated_at` DATETIME NOT NULL;
